0a54fc816f5c96201a506def2b34475d6f80261c
brandon <webarebrandy@gmail.com>
2024-05-12 15:37:01 -0600

    login funcionando, pero no muestra nombre de user

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:present_now/inicioalumnos/desconectado_screen.dart';
-import 'package:present_now/inicioalumnos/materias_screen.dart';
+import 'package:present_now/inicio_alumnos.dart';
+import 'package:present_now/inicio_maestros.dart';
 import 'package:present_now/providers/auth_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -17,42 +17,61 @@ class _LoginScreenState extends State<LoginScreen> {
   Future<void> _login() async {
     final authProvider = Provider.of<AuthProvider>(context, listen: false);
 
-    try {
-      final id = _idController.text;
-      if (id.length == 8 && int.tryParse(id) != null) {
-        // Si el ID tiene 8 caracteres y es un número, asumimos que es un número de control (alumno)
-        await authProvider.autenticarAlumno(
-          id,
-          _passwordController.text,
-        );
-      } else {
-        // Si no, lo tratamos como RFC (maestro)
-        await authProvider.autenticarMaestro(
-          id,
-          _passwordController.text,
-        );
-      }
+    final id = _idController.text.trim();
+    final password = _passwordController.text.trim();
 
-      // Redirigir según el rol
-      final role = authProvider.role;
-      if (role == 'alumno') {
+    if (id.isEmpty || password.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Por favor, completa todos los campos')),
+      );
+      return;
+    }
+
+    if (id.length > 13) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text(
+                'El número de control o RFC no puede tener más de 13 caracteres')),
+      );
+      return;
+    }
+
+    if (id.startsWith('C') && id.length == 9 || id.length == 8) {
+      // Si el ID comienza con 'C' y tiene 9 caracteres o solo son numeros y son 8 caracteres, asumimos que es un número de control válido
+      try {
+        await authProvider.autenticarAlumno(id, password);
+
         Navigator.pushReplacement(
           context,
-          MaterialPageRoute(builder: (context) => DesconectadoScreen()),
+          MaterialPageRoute(
+            builder: (context) => InicioAlumnos(),
+          ),
         );
-      } else if (role == 'maestro') {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => MateriasScreen()),
-        );
-      } else {
+      } catch (e) {
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Error: Rol desconocido')),
+          SnackBar(content: Text('Error de inicio de sesión: $e')),
         );
       }
-    } catch (e) {
+    } else if (RegExp(r'^[A-Z]{4}[0-9]{6}[A-Z0-9]{3}$').hasMatch(id) &&
+        id.length == 13) {
+      // Si el ID tiene el formato de RFC válido y tiene 13 caracteres, asumimos que es un RFC de maestro
+      try {
+        await authProvider.autenticarMaestro(id, password);
+
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => InicioMaestros(),
+          ),
+        );
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error de inicio de sesión: $e')),
+        );
+      }
+    } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error de inicio de sesión: $e')),
+        SnackBar(content: Text('Número de control o RFC inválido')),
       );
     }
   }
