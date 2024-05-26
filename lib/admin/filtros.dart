41b32b29d24648f92df66aa62fdf1fcc001b69f2
angelSantori <antonyo.da.mm@gmail.com>
2024-05-25 20:18:58 -0700

    Se creó el archivo filtros para admin y manejrar mejor las mejora

---
+++ lib/admin/filtros.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class UpperCaseTextInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    return newValue.copyWith(
+      text: newValue.text.toUpperCase(),
+      selection: newValue.selection,
+    );
+  }
+}
+
+class PasswordVisibilityToggle extends ChangeNotifier {
+  bool _obscureText = true;
+
+  bool get obscureText => _obscureText;
+
+  void togglePasswordVisibility() {
+    _obscureText = !_obscureText;
+    notifyListeners();
+  }
+}
