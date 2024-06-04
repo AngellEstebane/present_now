9e1d553c2873f9d99fd795f662e7020d4b7eec10
brandon <webarebrandy@gmail.com>
2024-06-03 23:38:54 -0600

    rediseño de pantalla revision de entradas maestros

--- lib/admin/revision_entrada_maestros.dart
+++ lib/admin/revision_entrada_maestros.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -13,18 +14,20 @@ class _ListAsistenciaMaestrosState extends State<ListAsistenciaMaestros> {
   DateTime? selectedDate;
   final dateController = TextEditingController();
   final aulaController = TextEditingController();
+  Timer? _timer;
+
   @override
   void initState() {
     super.initState();
-    fetchAsistencias().then((_) {
-      setState(() {
-        asistencias.forEach((asistencia) {
-          if (asistencia.entro) {
-            asistencia.entro = true;
-          }
-        });
-      });
-    });
+    fetchAsistencias();
+    _timer = Timer.periodic(Duration(minutes: 1),
+        (Timer t) => fetchAsistencias()); // Actualiza cada minuto
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
   }
 
   Future<void> fetchAsistencias({String? date, String? aula}) async {
@@ -78,17 +81,23 @@ class _ListAsistenciaMaestrosState extends State<ListAsistenciaMaestros> {
 
   Future<void> _updateAsistencia(
       MaestrosResponse asistencia, bool entro) async {
+    final formattedDate = asistencia.fechaHora.toIso8601String().split('T')[0];
     final url =
-        'https://proyecto-agiles.onrender.com/entrada/profesorcheck/${asistencia.fechaHora.toIso8601String().split('T')[0]}';
-    final response = await http.put(Uri.parse(url),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({'entro': entro}));
+        'https://proyecto-agiles.onrender.com/entrada/profesorcheck/$formattedDate';
+    final response = await http.put(
+      Uri.parse(url),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'entro': entro}),
+    );
 
     if (response.statusCode == 200) {
+      print('Asistencia actualizada exitosamente');
       setState(() {
         asistencia.entro = entro; // Actualizar el valor de entro
       });
     } else {
+      print(
+          'Error al actualizar la asistencia: ${response.statusCode}'); // Agrega esto para ver el error en la consola
       throw Exception('Failed to update asistencia');
     }
   }
@@ -97,90 +106,120 @@ class _ListAsistenciaMaestrosState extends State<ListAsistenciaMaestros> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lista de Asistencias de Maestros'),
+        backgroundColor: Color.fromARGB(255, 24, 81, 180), // solo azul
+        foregroundColor: Colors.white,
+        title: Text('Lista de Entrada de Maestros'),
+
+        elevation: 2,
+        shadowColor: Colors.transparent,
+        centerTitle: true,
+        clipBehavior: Clip.hardEdge,
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: dateController,
-              decoration: InputDecoration(
-                labelText: "Seleccionar Fecha",
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.calendar_today),
-                  onPressed: () => _selectDate(context),
-                ),
-              ),
-              readOnly: true,
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color.fromARGB(255, 24, 81, 180),
+              const Color.fromARGB(255, 26, 87, 136).withOpacity(0.5)
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: aulaController,
-              decoration: InputDecoration(
-                labelText: "Buscar por Aula",
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: _searchByAula,
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: isLoading
-                ? Center(child: CircularProgressIndicator())
-                : ListView.builder(
-                    itemCount: asistencias.length,
-                    itemBuilder: (context, index) {
-                      final asistencia = asistencias[index];
-                      return ListTile(
-                        title: Text('Profesor RFC: ${asistencia.profesorRfc}'),
-                        subtitle: Text(
-                            'Aula: ${asistencia.aula}\nFecha: ${asistencia.fechaHora}'),
-                        trailing: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(asistencia.entro ? 'Asistió' : 'No asistió'),
-                            Switch(
-                              value: asistencia.entro,
-                              onChanged: (bool newValue) {
-                                _updateAsistencia(asistencia, newValue);
-                              },
-                            ),
-                          ],
-                        ),
-                      );
-                    },
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: dateController,
+                decoration: InputDecoration(
+                  labelText: "Seleccionar Fecha",
+                  labelStyle: TextStyle(color: Colors.white),
+                  suffixIcon: IconButton(
+                    color: Colors.white,
+                    icon: Icon(Icons.calendar_today),
+                    onPressed: () => _selectDate(context),
                   ),
-          ),
-        ],
+                ),
+                readOnly: true,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: aulaController,
+                decoration: InputDecoration(
+                  labelText: "Buscar por Aula",
+                  labelStyle: TextStyle(color: Colors.white),
+                  suffixIcon: IconButton(
+                    color: Colors.white,
+                    icon: Icon(Icons.search),
+                    onPressed: _searchByAula,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: isLoading
+                  ? Center(child: CircularProgressIndicator())
+                  : ListView.builder(
+                      itemCount: asistencias.length,
+                      itemBuilder: (context, index) {
+                        final asistencia = asistencias[index];
+                        return Card(
+                          child: ListTile(
+                            title:
+                                Text('Profesor RFC: ${asistencia.profesorRfc}'),
+                            subtitle: Text(
+                                'Aula: ${asistencia.aula}\nFecha: ${asistencia.fechaHora}'),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(asistencia.entro
+                                    ? 'Asistió'
+                                    : 'No asistió'),
+                                Switch(
+                                  value: asistencia.entro,
+                                  onChanged: (bool newValue) {
+                                    _updateAsistencia(asistencia, newValue);
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
 class MaestrosResponse {
+  final int id;
   final String profesorRfc;
   final String aula;
   final DateTime fechaHora;
   bool entro; // Cambiar el tipo de entro a booleano
 
   MaestrosResponse({
+    required this.id,
     required this.profesorRfc,
     required this.aula,
     required this.fechaHora,
-    this.entro = false, // Inicializar entro con false (no asistió) por defecto
+    required this.entro, // No inicializar aquí, se debe inicializar en fromJson
   });
 
   factory MaestrosResponse.fromJson(Map<String, dynamic> json) {
     return MaestrosResponse(
+      id: json['id'],
       profesorRfc: json['ProfesorRFC'],
       aula: json['aula'],
       fechaHora: DateTime.parse(json['fechaConHora']),
-      entro: json['entro'] ??
-          false, // Utilizar false como valor por defecto si no hay valor en el JSON
+      entro: json['Entro'] == 1, // Convertir el valor numérico a booleano
     );
   }
 }
